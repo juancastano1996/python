b3c8699b39cd06aee41abe4fab422f5db2c15d15
CamiloCastano <jc_cp96@hotmail.com>
2024-02-26 21:21:34 -0500

    Actualización V 2.0

--- app/core/erp/models.py
+++ app/core/erp/models.py
@@ -1,51 +1,80 @@
 from django.db import models
 from datetime import datetime
 
-class Type(models.Model):
-    name = models.CharField(max_length = 150, verbose_name = 'nombres', unique = True)
+from core.erp.choices import gender_choices
 
-    def __str__(self):
-        return self.name
-
-    class Meta:
-        verbose_name        = 'Tipo'
-        verbose_name_plural = 'Tipos'
-        db_table            = 'tipo'
-        ordering            = ['id']
 
 class Category(models.Model):
-    name = models.CharField(max_length = 150, verbose_name = 'nombres')
+    name = models.CharField(max_length=150, verbose_name='Nombre', unique=True)
 
     def __str__(self):
         return self.name
 
     class Meta:
-        verbose_name        = 'Categoria'
+        verbose_name = 'Categoria'
         verbose_name_plural = 'Categorias'
-        db_table            = 'categoria'
-        ordering            = ['id']
+        ordering = ['id']
 
 
-class Employee(models.Model):
-    categ         = models.ManyToManyField(Category)
-    type          = models.ForeignKey(Type, on_delete = models.CASCADE)
-    names         = models.CharField(max_length = 150, verbose_name = 'nombres')
-    num_documento = models.CharField(max_length = 10, verbose_name = 'numero_documento', unique = True)
-    date_joined   = models.DateField(default = datetime.now, verbose_name = 'fecha_registro')
-    date_creation = models.DateTimeField(auto_now = True, verbose_name = 'fecha_creacion')
-    date_updated  = models.DateTimeField(auto_now_add = True, verbose_name = 'fecha_creacion')
-    age           = models.PositiveBigIntegerField(default = 0)
-    salary        = models.DecimalField(default = 0.00, max_digits = 9, decimal_places = 2)
-    state         = models.BooleanField(default = True)
-    gender        = models.CharField(max_length = 50)
-    avatar        = models.ImageField(upload_to = 'avatar/%y/%m/%d', null = True, blank = True)
-    cvitae        = models.FileField(upload_to = 'cvitae/%y/%m/%d', null = True, blank = True)
+class Product(models.Model):
+    name  = models.CharField(max_length=150, verbose_name='Nombre', unique=True)
+    cate  = models.ForeignKey(Category, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='product/%Y/%m/%d', null=True, blank=True)
+    pvp   = models.DecimalField(default=0.00, max_digits=9, decimal_places=2)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Producto'
+        verbose_name_plural = 'Productos'
+        ordering = ['id']
+
+
+class Client(models.Model):
+    names    = models.CharField(max_length=150, verbose_name='Nombres')
+    surnames = models.CharField(max_length=150, verbose_name='Apellidos')
+    dni      = models.CharField(max_length=10, unique=True, verbose_name='Dni')
+    birthday = models.DateField(default=datetime.now, verbose_name='Fecha de nacimiento')
+    address  = models.CharField(max_length=150, null=True, blank=True, verbose_name='Dirección')
+    sexo     = models.CharField(max_length=10, choices=gender_choices, default='male', verbose_name='Sexo')
 
     def __str__(self):
         return self.names
 
     class Meta:
-        verbose_name        = 'Empleado'
-        verbose_name_plural = 'Empleados'
-        db_table            = 'empleado'
-        ordering            = ['id']
+        verbose_name = 'Cliente'
+        verbose_name_plural = 'Clientes'
+        ordering = ['id']
+
+
+class Sale(models.Model):
+    cli         = models.ForeignKey(Client, on_delete=models.CASCADE)
+    date_joined = models.DateField(default=datetime.now)
+    subtotal    = models.DecimalField(default=0.00, max_digits=9, decimal_places=2)
+    iva         = models.DecimalField(default=0.00, max_digits=9, decimal_places=2)
+    total       = models.DecimalField(default=0.00, max_digits=9, decimal_places=2)
+
+    def __str__(self):
+        return self.cli.names
+
+    class Meta:
+        verbose_name = 'Venta'
+        verbose_name_plural = 'Ventas'
+        ordering = ['id']
+
+
+class DetSale(models.Model):
+    sale     = models.ForeignKey(Sale, on_delete=models.CASCADE)
+    prod     = models.ForeignKey(Product, on_delete=models.CASCADE)
+    price    = models.DecimalField(default=0.00, max_digits=9, decimal_places=2)
+    cant     = models.IntegerField(default=0)
+    subtotal = models.DecimalField(default=0.00, max_digits=9, decimal_places=2)
+
+    def __str__(self):
+        return self.prod.name
+
+    class Meta:
+        verbose_name = 'Detalle de Venta'
+        verbose_name_plural = 'Detalle de Ventas'
+        ordering = ['id']
--- app/core/erp/tests.py
+++ app/core/erp/tests.py
@@ -1,30 +1,9 @@
-from django.test import TestCase
-from app.wsgi import *
-from core.erp.models import Type
+from config.wsgi import *
+from core.erp.models import *
 
-#Listar
-#query = Type.objects.all()
-#print(query)
+# LISTAR
 
-#Insertar
-#t = Type()
-#t.name = 'Prueba'
-#t.save()
+print(Category.objects.all())
 
-#Editar
-#try:
-#    t = Type.objects.get(id = 2)
-#    t.name = 'Prueba'
-#    t.save()
-#    #print(t.name)
-#except Exception as e:
-#    print(e)
-
-#Eliminar
-#t = Type.objects.get(pk = 1)
-#t.delete()
-#print(query)
-
-
-for i in Type.objects.filter(name__endswith = 'a'):
-    print(i.name)
+for i in Category.objects.filter():
+    print(i)
