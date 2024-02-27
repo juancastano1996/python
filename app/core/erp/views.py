e577470c55fd4a69696cb756597fe2d63a2c96c4
CamiloCastano <jc_cp96@hotmail.com>
2024-02-26 23:15:00 -0500

    Crea la primer vista

--- app/core/erp/views.py
+++ app/core/erp/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
 
-# Create your views here.
+def myfirstview(request):
+    data = {
+        'name' : 'Camilo'
+    }
+    #return JsonResponse(data)
+
+    return render(request, 'index.html')
